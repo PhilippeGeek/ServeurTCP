b5fcf189ce43b7780d5b5522f82bc93675239316
Philippe Vienne <philippe@vienne.me>
2016-02-19 08:56:02 +0100

    Setup a socket connection on server

--- src/serveur.c
+++ src/serveur.c
@@ -1,5 +1,15 @@
-//
-// Created by tmoutier on 19/02/16.
-//
 
-#include "serveur.h"
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+int main(){
+    int socket_desc = socket(PF_INET, SOCK_STREAM, 0);
+    if(socket_desc == -1){
+        fprintf(stderr, "Can not open an IPv4 socket ! Closing now.");
+        exit(1);
+    }
+    printf("Yeah ! Socket is opened with descriptor %d", socket_desc);
+    return 0;
+}
