c7b62c7d4a771aac9ce81791834720f61cad52a1
Philippe Vienne <philippe@vienne.me>
2016-02-19 09:55:13 +0100

    Bind the server correctly

--- src/serveur.c
+++ src/serveur.c
@@ -1,15 +1,51 @@
-
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include "constants.h"
 #include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include "serveur.h"
+
+int running = 1;
 
 int main(){
-    int socket_desc = socket(PF_INET, SOCK_STREAM, 0);
+    int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     if(socket_desc == -1){
         fprintf(stderr, "Can not open an IPv4 socket ! Closing now.");
-        exit(1);
+        return 1;
     }
+
+    setup_server(socket_desc);
+
     printf("Yeah ! Socket is opened with descriptor %d\n", socket_desc);
+
+    while(running){
+
+    }
     return 0;
+}
+
+struct sockaddr_in init_server_descriptor() {
+    struct sockaddr_in addr;
+    memset((char*)&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET; // We are talking about IPv4
+    addr.sin_port = htons(SERVER_PORT); // See constants.h for value
+    addr.sin_addr.s_addr = INADDR_ANY; // Listen on 0.0.0.0
+    return addr;
+}
+
+void setup_server(int socket_desc) {
+    enable_reuse_socket(socket_desc);
+    bind_server_socket(socket_desc);
+}
+
+void bind_server_socket(int socket_desc) {
+    struct sockaddr_in addr = init_server_descriptor();
+    bind(socket_desc, (const struct sockaddr *) &addr, sizeof(addr)); // Bind the socket to the address
+}
+
+void enable_reuse_socket(int socket_desc) {
+    int reuse = REUSE_SOCKETS;
+    setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int)); // Setup to allow socket reopen
 }
--- src/serveur.h
+++ src/serveur.h
@@ -5,4 +5,9 @@
 #ifndef SERVEURTCP_SERVEUR_H
 #define SERVEURTCP_SERVEUR_H
 
+struct sockaddr_in init_server_descriptor();
+void enable_reuse_socket(int socket_desc);
+void bind_server_socket(int socket_desc);
+void setup_server(int socket_desc);
+
 #endif //SERVEURTCP_SERVEUR_H
