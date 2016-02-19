8e1865e51ac6b8399f33de808b45fe484ac3fdc7
Theo Moutier <moutier.theo@gmail.com>
2016-02-19 10:39:38 +0100

    Setup a socket connection on client

--- src/client.c
+++ src/client.c
@@ -1,11 +1,14 @@
-//
-// Created by tmoutier on 19/02/16.
-//
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "client.h"
+#include "constants.h"
+
+void enable_reuse_socket(int socket_desc);
 
 int main(){
     int socket_desc;
@@ -15,6 +18,23 @@ int main(){
         printf("Socket creation error\n");
         exit(1);
     }
+    struct sockaddr_in addr;
+    memset((char*)&addr, 0, sizeof(addr));
+    addr.sin_family= AF_INET;
+    addr.sin_port=htons(SERVER_PORT);
+    struct in_addr listen_ip;
+    inet_aton("127.0.0.1", &listen_ip);
+    addr.sin_addr = listen_ip;
+
+    enable_reuse_socket(socket_desc);
+
     printf("Socket is opened with description: %d\n", socket_desc);
+
+
     return 0;
 }
+
+void enable_reuse_socket(int socket_desc) {
+        int reuse= REUSE_SOCKETS;
+        setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    }
