22f4459a2c26c3fe5f75da03ff8e0528a6b308c4
Theo Moutier <moutier.theo@gmail.com>
2016-02-19 08:34:43 +0100

    Adding printing function

--- src/client.c
+++ src/client.c
@@ -1,5 +1,12 @@
 //
 // Created by tmoutier on 19/02/16.
 //
-
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include "client.h"
+
+int main(){
+    printf("Test");
+    return 0;
+}
