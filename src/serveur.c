75c1564ca4baa633020c134551e99d28250587f0
Theo Moutier <moutier.theo@gmail.com>
2016-03-04 08:49:12 +0100

    server now accepts ports as arguments

--- src/serveur.c
+++ src/serveur.c
@@ -12,18 +12,27 @@
 int running = 1;
 
 int main(int argc, char* argv[]){
+    int port;
     switch(argc){
-        case 0: printf("Your port number will be 3456\n");
-                
+        case 1: printf("No port number defined: Your port number will be 3456\n");
+                port=3456;
+                break;
+
+        case 2: printf("Your port number is %s\n", argv[1]);
+                port=atoi(argv[1]);
+                break;
+
+        default: printf("Too many arguments, try again with only one\n");
+                 return 1;
     }
-        
+
     int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     if(socket_desc == -1){
         fprintf(stderr, "Can not open an IPv4 socket ! Closing now.");
         return 1;
     }
 
-    setup_server(socket_desc);
+    setup_server(socket_desc, port);
 
     listen(socket_desc, 128);
 
@@ -48,24 +57,24 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
-struct sockaddr_in init_server_descriptor() {
+struct sockaddr_in init_server_descriptor(int port) {
     struct sockaddr_in addr;
     memset((char*)&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET; // We are talking about IPv4
-    addr.sin_port = htons(SERVER_PORT); // See constants.h for value
+    addr.sin_port = htons((uint16_t) port); // See constants.h for value
     struct in_addr listen_ip;
     inet_aton("0.0.0.0", &listen_ip);
     addr.sin_addr = listen_ip; // Listen on 0.0.0.0
     return addr;
 }
 
-void setup_server(int socket_desc) {
+void setup_server(int socket_desc, int port) {
     enable_reuse_socket(socket_desc);
-    bind_server_socket(socket_desc);
+    bind_server_socket(socket_desc, port);
 }
 
-void bind_server_socket(int socket_desc) {
-    struct sockaddr_in addr = init_server_descriptor();
+void bind_server_socket(int socket_desc, int port) {
+    struct sockaddr_in addr = init_server_descriptor(port);
     bind(socket_desc, (const struct sockaddr *) &addr, sizeof(addr)); // Bind the socket to the address
 }
 
--- src/serveur.h
+++ src/serveur.h
@@ -5,9 +5,9 @@
 #ifndef SERVEURTCP_SERVEUR_H
 #define SERVEURTCP_SERVEUR_H
 
-struct sockaddr_in init_server_descriptor();
+struct sockaddr_in init_server_descriptor(int port);
 void enable_reuse_socket(int socket_desc);
-void bind_server_socket(int socket_desc);
-void setup_server(int socket_desc);
+void bind_server_socket(int socket_desc, int port);
+void setup_server(int socket_desc, int port);
 
 #endif //SERVEURTCP_SERVEUR_H
